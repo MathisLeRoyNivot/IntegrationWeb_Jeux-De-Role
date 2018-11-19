2a85af1ed04ad2aa2b751a48e8b31fba88b9ccad
Mathis Le Roy-Nivot <mathis.leroynivot@ynov.com>
2018-11-19 15:54:57 +0100

    Ajout media queries 2.0 modifcations affichage mobile

--- js/main.js
+++ js/main.js
@@ -14,3 +14,19 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+// BURGER MENU
+$('.burger-menu').on('click', function () {
+    $(this).toggleClass("burger-menu--opened");
+    $(this).toggleClass("burger-menu--closed");
+  });
+  
+  
+  function myFunction() {
+    var x = document.getElementById("navbarTop");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  } 
